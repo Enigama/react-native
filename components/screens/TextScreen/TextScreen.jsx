f0826c39073df03e8989c4e40715f9cd917995bf
Enigana <zk@seadora.com.ua>
2020-04-26 17:39:27 +0300

    Text screen add some message for password and add some style

--- components/screens/TextScreen/TextScreen.jsx
+++ components/screens/TextScreen/TextScreen.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from "react-native";
 
 const TextScreen = () => {
-  const [text, setText] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View>
-      <Text>Text screen</Text>
+    <View style={{position: 'relative'}}>
+      <Text>Enter password:</Text>
       <TextInput
         style={style.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={text}
-        onChangeText={(val) => setText(val)}
+        value={password}
+        onChangeText={(val) => setPassword(val)}
       />
+
+      {
+        password.length < 4 ? <Text style={style.errorText}>The password length is my be big than 4 chars!!!!</Text> : null
+      }
+      <View style={style.absoluteBlock}><Text>Absolute</Text></View>
     </View>
   )
 };
@@ -22,6 +27,12 @@ const style = StyleSheet.create({
     margin: 15,
     borderColor: 'black',
     borderWidth: 1,
+  },
+  errorText: {
+    color: 'red',
+  },
+  absoluteBlock: {
+    backgroundColor: 'yellow',
   }
 });
 
